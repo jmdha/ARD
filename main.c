1ebb754a0c76eabb3c7e27b7200044582d381635
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-06-20 13:48:20 +0200

    refactor: introduce gloop

--- main.c
+++ main.c
@@ -8,16 +8,17 @@
 
 #define max(x,y) (((x) >= (y)) ? (x) : (y))
 #define min(x,y) (((x) <= (y)) ? (x) : (y))
+// I don't know whether I like this.
+#define gloop(w, h) for (int x = 0; x < w; x++) for (int y = 0; y < h; y++)
 
-uint gmax(const uint* buf, int w, int h) {
+int gmax(const int* buf, int w, int h) {
 	int m = 0;
-	for (int x = 0; x < w; x++)
-		for (int y = 0; y < h; y++)
-			m = max(m, buf[y * w + x]);
+	gloop(w, h)
+		m = max(m, buf[y * w + x]);
 	return m;
 }
 
-uint pval(const uint* buf, int w, int h, int x, int y) {
+int pval(const int* buf, int w, int h, int x, int y) {
 	for (int i = 0; i < min(abs(w - x), abs(h - y)); i++)
 		if (!buf[(y + i) * w + (x + i)] ||
 		    !buf[(y + i) * w + (x - i)] ||
@@ -27,10 +28,9 @@ uint pval(const uint* buf, int w, int h, int x, int y) {
 	return min(abs(w - x), abs(h - y));
 }
 
-void prox(uint* out, const uint* buf, int w, int h) {
-	for (int x = 0; x < w; x++)
-		for (int y = 0; y < h; y++)
-			out[y * w + x] = pval(buf, w, h, x, y);
+void prox(int* out, const int* buf, int w, int h) {
+	gloop(w, h)
+		out[y * w + x] = pval(buf, w, h, x, y);
 }
 
 struct cor {
@@ -48,23 +48,22 @@ int cor_comp(const void* a, const void* b) {
 		return 0;
 }
 
-uint grid_vals(struct cor** cors, const uint* buf, uint w, uint h) {
+uint grid_vals(struct cor** cors, const int* buf, int w, int h) {
 	*cors = malloc(w * h * sizeof(struct cor));
 	uint i = 0;
-	for (uint x = 0; x < w; x++)
-		for (uint y = 0; y < h; y++)
-			if (buf[y * w + x] != 0) {
-				(*cors)[i].x = x;
-				(*cors)[i].y = y;
-				(*cors)[i].v = buf[y * w + x];
-				i++;
-			}
+	gloop(w, h)
+		if (buf[y * w + x]) {
+			(*cors)[i].x = x;
+			(*cors)[i].y = y;
+			(*cors)[i].v = buf[y * w + x];
+			i++;
+		}
 	*cors = realloc(*cors, i * sizeof(struct cor));
 	return i;
 }
 
-void sect_gen(uint* out, const struct cor* cors, int c, int w, int h) {
-	memset(out, 0, w * h * sizeof(uint));
+void sect_gen(int* out, const struct cor* cors, int c, int w, int h) {
+	memset(out, 0, w * h * sizeof(int));
 	int s = 1;
 	for (int i = 0; i < c; i++) {
 		const struct cor* cor = &cors[i];
@@ -79,25 +78,24 @@ void sect_gen(uint* out, const struct cor* cors, int c, int w, int h) {
 	}
 }
 
-void sect(uint* out, const uint* buf, uint w, uint h) {
+void sect(int* out, const int* buf, uint w, uint h) {
 	struct cor* cors;
-	const uint c = grid_vals(&cors, buf, w, h);
+	const int c = grid_vals(&cors, buf, w, h);
 	qsort(cors, c, sizeof(struct cor), cor_comp);
 	sect_gen(out, cors, c, w, h);
 	free(cors);
 }
 
-int save_img(const char* name, const uint* buf, uint w, uint h) {
+int save_img(const char* name, const int* buf, int w, int h) {
 	unsigned char* out = calloc(w * h, sizeof(unsigned char));
-	uint m = gmax(buf, w, h);
+	int m = gmax(buf, w, h);
 	printf("%d\n", m);
-	for (uint x = 0; x < (uint) w; x++)
-		for (uint y = 0; y < (uint) h; y++)
-			out[y * w + x] = 255 * ((float) buf[y * w + x] / m);
+	gloop(w, h)
+		out[y * w + x] = 255 * ((float) buf[y * w + x] / m);
 	return stbi_write_png(name, w, h, 1, out, w * sizeof(unsigned char));
 }
 
-int main(int, char** argv) {
+int main(__attribute__((unused)) int argc, char** argv) {
 	stbi_write_png_compression_level = 0;
 	int w, h, n;
 	unsigned char* img_buf = stbi_load(argv[1], &w, &h, &n, 1);
@@ -105,7 +103,7 @@ int main(int, char** argv) {
 		printf("Failed to load file\n");
 		return 1;
 	}
-	uint* buf = malloc(w * h * sizeof(uint));
+	int* buf = malloc(w * h * sizeof(uint));
 	for (int i = 0; i < w * h; i++)
 		buf[i] = img_buf[i];
 	prox(buf, buf, w, h);
